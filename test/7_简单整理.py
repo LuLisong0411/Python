948382222541adb805cf5a1959a24de6165e7c23
陆力松 <lulisong0411@163.com>
2020-01-04 23:50:28 +0800

    简单梳理几种数据类型

---
+++ test/7_简单整理.py
@@ -0,0 +1,31 @@
+# 一、数字
+# int(..)
+# 二、字符串
+# replace/find/join/strip/startswith/split/upper/lower/format
+# tempalte = "i am {name}, age : {age}"
+# # v = tempalte.format(name='alex',age=19)
+# v = tempalte.format(**{"name": 'alex','age': 19})
+# print(v)
+# 三、列表
+# append、extend、insert
+# 索引、切片、循环
+# 四、元组
+# 忽略
+# 索引、切片、循环         以及元素不能被修改
+# 五、字典
+# get/update/keys/values/items
+# for,索引
+
+# dic = {
+#     "k1": 'v1'
+# }
+# 判断字典中是否存在key或value
+# v = "k1" in dic
+# print(v)
+
+# v = "v1" in dic.values()
+# print(v)
+# 六、布尔值
+# 0 1
+# bool(...)
+# None ""  () []  {} 0 ==> False
